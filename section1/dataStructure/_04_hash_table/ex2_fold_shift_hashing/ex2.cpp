b9b0ae27344ae980a344a4ebbb98c0ec32d8a981
Salamander <mokmaard646@gmail.com>
2023-08-22 21:14:09 +0700

    update open hashing

--- section1/dataStructure/_04_hash_table/ex2_fold_shift_hashing/ex2.cpp
+++ section1/dataStructure/_04_hash_table/ex2_fold_shift_hashing/ex2.cpp
@@ -5,36 +5,154 @@ using namespace std;
 #define ARRAYSIZE 20
 #define STUDENT_ID_SIZE 14
 
-unsigned long hashFunction( char* key ) {
+class Node {
+public : 
+  string stdId;
+  Node* next;
+  
+  Node( string value ) : stdId ( value ), next( nullptr ) {} 
+};
 
-  key += 1;
-  int i = 0;
-  int sum = 0;
-  string s;
+class LinkedList  {
+public : 
+  Node* head;
+  
+  LinkedList() : head( nullptr ) {}
+  
+  void insert( string numberId ) {
+    Node* newNode = new Node( numberId );
 
-  while( *( key ) != '\0' ) {
+    if ( head == nullptr ) {
+      head = newNode;
 
-    if ( i == 4 ) {
-      sum += stoi(s);
-      i = 0;
-      s.clear();
-      
-    } else {
-      s += *( key );
-      key += 1;
-      i++;
+    } else { 
+      Node* current = head;
+
+      while( current -> next != nullptr ) {
+        current = current -> next;
+      }
+      current -> next = newNode;
+
     }
+  } 
+
+  void display() {
+
+    if ( head == nullptr ) {
+      cout << "-1" << endl;
+      return;
+    }
+
+    Node* current = head;
+
+    while( current != nullptr ) {
+      cout << current -> stdId << "->";
+      current = current -> next;
+    }
+
+    cout << endl;
   }
 
-  // cout << s << endl;
-  sum += stoi(s);
-  return ( sum ) % ARRAYSIZE;
+  void findNode( string studentIdToFind ) {
+      Node* curr = head;
+      while( curr != nullptr ) {
+        if ( curr -> stdId == studentIdToFind ) {
+            cout << "id : " << curr -> stdId << endl;
+        }
+        curr = curr -> next;
+      }
+  }
+
+  void deleteNode( string numberIdToRemove ) {
+    Node* temp;
+
+    if ( head -> stdId == numberIdToRemove ) {
+      temp = head;
+      head = head -> next;
+      delete temp;
+      return;
+    }
+
+    Node* curr = head;
+    temp = curr;
+
+    curr = curr -> next;
+
+    while ( curr -> next != nullptr ) {
+
+      if ( curr -> stdId == numberIdToRemove ) {
+        temp -> next = curr -> next;
+        delete curr;
+        return;
+      }
+
+      curr = curr -> next;
+    }
+    
+  }
+};
+
+unsigned long hashFunction( char* key ) {
+
+    key += 1;
+    int i = 0;
+    int sum = 0;
+    string s;
+
+    while( *key != '\0' ) {
+
+      if ( i == 4 ) {
+        sum += stoi(s);
+        i = 0;
+        s.clear();
+        
+      } else {
+        s += *( key );
+        key += 1;
+        i++;
+      }
+    }
+    sum += stoi(s);
+    return ( sum ) % ARRAYSIZE;
 }
 
+
 int main () {
-  char key[STUDENT_ID_SIZE] = "6504062620159";
-  // unsigned long index = hash( key );
-  cout << hashFunction( key ) << endl;
-  cout << 11461 % 20 << endl;
-  cout << 5040 + 6262 + 159;
+  char key[STUDENT_ID_SIZE];
+  LinkedList array[ARRAYSIZE];
+
+  char command;
+
+  while ( command != 'E' ) {
+    cin >> command;
+
+    if ( command == 'A' ) {
+      cin >> key;
+      int index = hashFunction( key );
+      array[ index ].insert( key );
+    }
+
+    if ( command == 'P' ) {
+      int i = 0;
+      while( i < 20 ) {
+        array[ i ].display();
+        i++;
+      }
+    }
+
+    if ( command == 'D' ) {
+      cin >> key;
+      int index = hashFunction( key );
+      array[ index ].deleteNode( key );
+    }
+
+    if ( command == 'F' ) {
+      cin >> key;
+      int index = hashFunction( key );
+      array[ index ].findNode( key );
+    }
+
+    cout << endl;
+  }
+
 }
