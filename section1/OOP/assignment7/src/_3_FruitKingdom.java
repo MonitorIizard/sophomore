952781ce90614b0839a2686c45a28b2a970ec234
Salamander <mokmaard646@gmail.com>
2023-09-15 22:06:20 +0700

    upload assignment7

--- section1/OOP/assignment7/src/_3_FruitKingdom.java
+++ section1/OOP/assignment7/src/_3_FruitKingdom.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class _3_FruitKingdom {
+public class _3_FruitKingdom {
+    public static void main( String[] args ) {
+        Fruit fruit = new GoldenDelicious();
+        Orange orange = new Orange();
+
+        orange.MakeOrangeJuice();
+
+    }
+}
+
+class Fruit {
+
+
+}
+class Apple extends Fruit {
+    public void MakeApple() {
+        System.out.println("makeApple");
+    }
+}
+
+class Orange extends Fruit {
+    public void MakeOrangeJuice() {
+        System.out.println("makeOrangeJuice");
+    }
+}
+
+class GoldenDelicious extends Apple {
+
+}
+
+class Mcintouch extends Apple {
+
 }
