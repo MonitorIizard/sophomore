18e57e567c2aa6e2e909835282258c0407baf4ef
Salamander <mokmaard646@gmail.com>
2023-09-28 16:57:02 +0700

    upload binary tree

--- section1/dataStructure/_06_BinaryTree/binarySearchTree.cpp
+++ section1/dataStructure/_06_BinaryTree/binarySearchTree.cpp
@@ -8,10 +8,8 @@ class BTS {
 private : 
   int data;
   BTS *left, *right;
-  
 
 public :
-  Queue *q = new Queue();
   
   //default constructor
   BTS() {
@@ -54,9 +52,9 @@ public :
 
   void preorder( BTS *root ) {
     if( !root ) return;
-    preorder( root->right );
     cout << root -> data << ",";
     preorder( root->left );
+    preorder( root->right );
   }
 
   void postorder( BTS *root ) {
@@ -66,16 +64,133 @@ public :
     cout << root -> data << ",";
   }
 
-  void breadthFirstTraversal( BTS *root ) {
-    q->enqueue(root);
-    for( int i = 0; i < q->length; i++ ) {
-      cout<<i;
-    }
-    
-  }
+  void breadthFirstTraversal( BTS *root, Queue *queue );
 
   int getData() {
     return data;
+  }
+
+//   BTS* deleteNode(BTS* root, int k)
+// {
+//     // Base case
+//     if (root == NULL)
+//         return root;
+ 
+//     // Recursive calls for ancestors of
+//     // node to be deleted
+//     if (root->data > k) {
+//         root->left = deleteNode(root->left, k);
+//         return root;
+//     }
+//     else if (root->data < k) {
+//         root->right = deleteNode(root->right, k);
+//         return root;
+//     }
+ 
+//     // We reach here when root is the node
+//     // to be deleted.
+ 
+//     // If one of the children is empty
+//     if (root->left == NULL) {
+//         BTS* temp = root->right;
+//         delete root;
+//         return temp;
+//     }
+//     else if (root->right == NULL) {
+//         BTS* temp = root->left;
+//         delete root;
+//         return temp;
+//     }
+ 
+//     // If both children exist
+//     else {
+ 
+//         BTS* succParent = root;
+ 
+//         // Find successor
+//         BTS* succ = root->right;
+//         while (succ->left != NULL) {
+//             succParent = succ;
+//             succ = succ->left;
+//         }
+ 
+//         // Delete successor.  Since successor
+//         // is always left child of its parent
+//         // we can safely make successor's right
+//         // right child as left of its parent.
+//         // If there is no succ, then assign
+//         // succ->right to succParent->right
+//         if (succParent != root)
+//             succParent->left = succ->right;
+//         else
+//             succParent->right = succ->right;
+ 
+//         // Copy Successor Data to root
+//         root->data = succ->data;
+ 
+//         // Delete Successor and return root
+//         delete succ;
+//         return root;
+//     }
+// }
+  BTS* deleteNode( BTS* root, int dataToDel ) {
+    if( !root ) {
+      return root;
+    }
+
+    if ( root->data > dataToDel ) {
+      root->left = deleteNode( root->left, dataToDel );
+      return root; //return that node to join
+    }
+    if ( root->data < dataToDel ) {
+      root->right = deleteNode( root->right, dataToDel );
+      return root;
+    }
+
+    if ( root -> left == NULL ) {
+      BTS* temp = root -> right;
+      delete root;
+      return temp;
+    
+    } else if ( root -> right == NULL ) {
+      BTS* temp = root -> left;
+      delete root;
+      return temp;
+
+    } else {
+      
+      BTS* SuccParent = root;
+      BTS* Succ = SuccParent->right;
+      while( Succ->left != NULL ) {
+        SuccParent = Succ;
+        Succ = Succ->left;
+      }
+
+      if ( SuccParent != root ) {
+        SuccParent->left = Succ->right;
+      } else {
+        SuccParent->right = Succ->right; 
+      }
+
+      delete Succ;
+      return root;
+
+    }
+  }
+
+  BTS* search( BTS *root, int valTOFind ) {
+    if( !root ) {
+      return NULL;
+    }
+
+    if( root->data == valTOFind ) return root;
+
+    if( valTOFind > root->data ) {
+      return search( root->right, valTOFind );
+    } else {
+      return search( root->left, valTOFind );
+    }
+
   }
 };
 
@@ -100,6 +215,7 @@ public:
   }
 
   void enqueue( BTS *node ) {
+    length++;
     if( !root ) { 
       root = new Node( node );
       return; 
@@ -111,19 +227,18 @@ public:
     while( curr->next != NULL ) {
       curr = curr->next;
     }
-
-    length++;
     curr->next = newNode;
   }
 
-  int dequeue( ) {
+  BTS* dequeue( ) {
+    if( !root ) return 0;
     Node *temp = root;
     root = root->next;
 
-    int tempData = temp->node->getData();
+    BTS *tempNode = temp->node;
     delete temp;
     length--;
-    return tempData;
+    return tempNode;
   }
 
   void print( ) {
@@ -136,25 +251,83 @@ public:
   
 };
 
+void BTS::breadthFirstTraversal( BTS *root, Queue *queue ) {
+  if( !root ) return;
+
+  queue->enqueue(root);
+  int numberOfChildAtLevel = 1;
+  int temp = 0;
+
+  while( queue -> length != 0 ) {
+    BTS* dequeueNode = queue->dequeue();
+    cout << dequeueNode->getData() << ",";
+    numberOfChildAtLevel -= 1;
+    
+    if(dequeueNode->left) {
+      queue->enqueue(dequeueNode->left);
+      temp++;
+    }
+    
+    if (dequeueNode->right) {
+      queue->enqueue(dequeueNode->right);
+      temp++;
+    }
+
+    if ( numberOfChildAtLevel == 0 ) {
+      cout << "|";
+      numberOfChildAtLevel = temp;
+      temp = 0;
+    };
+  }
+
+  delete queue;
+  }
+
 
 int main( ) {
   BTS* root = NULL;
-  BTS binarySearchTree = NULL; 
-  root = binarySearchTree.insert( root, 10 );
-  binarySearchTree.insert( root, 7 );
-  binarySearchTree.insert( root, 13 );
-  binarySearchTree.insert( root, 2 );
-  binarySearchTree.insert( root, 9 );
-  binarySearchTree.insert( root, 20 );
-  binarySearchTree.insert( root, 11 );
-  binarySearchTree.inorder( root );
-  cout << "\n";
-  binarySearchTree.preorder( root );
-  cout << "\n";
-  binarySearchTree.postorder( root );
-  cout << "\n";
-  binarySearchTree.breadthFirstTraversal( root );
+  BTS binarySearchTree; 
 
+  char command;
+  while (command != 'x' ) 
+  {
+    cin >> command;
+    if( command == 'a' ) {
+      int data;
+      cin >> data;
+      if ( !root ) {
+        root = binarySearchTree.insert( root, data );
+      } else {
+        binarySearchTree.insert( root, data );
+      }
+    }
 
+    if( command == 'b' ) {
+      Queue *q = new Queue();
+      binarySearchTree.breadthFirstTraversal( root, q );
+      cout << "\n";
+    }
+
+    if( command == 'i' ) {
+      binarySearchTree.inorder( root );
+      cout << "\n";
+    }
+
+    if( command == 'p' ) {
+      binarySearchTree.preorder( root );
+      cout << "\n";
+    }
+
+    if( command == 't' ) {
+      binarySearchTree.postorder( root );
+      cout << "\n";
+    }
+
+    if ( command == 'd' ) {
+      int dataToDelete;
+      cin >> dataToDelete;
+      binarySearchTree.deleteNode( root , dataToDelete );
+    }
+  }
   return 0;
 }
